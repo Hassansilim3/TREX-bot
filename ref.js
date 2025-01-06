aeee0bf9928f3e144cc8c4f5bdf7663cbe4833cd
Hassansilim3 <168602108+Hassansilim3@users.noreply.github.com>
2025-01-06 18:19:23 +0200

    ref.js

--- ref.js
+++ ref.js
@@ -84,9 +84,9 @@ const topUpRef = async (e) => {
   const userPointsRef = ref(db, `users/${refUsername}/points`);
   get(userPointsRef).then((snapshot) => {
      console.log(snapshot.val());
-    const currentPoints = snapshot.exists() ? snapshot.val() : 0;
+    const score = snapshot.exists() ? snapshot.val() : 0;
     update(ref(db, `users/${refUsername}`), {
-      score: Number(currentPoints) + 100,
+      Points: Number(score) + 100,
     });
     e.target.parentNode.remove();
   });
